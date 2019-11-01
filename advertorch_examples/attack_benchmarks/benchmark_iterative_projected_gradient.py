5ffb516754c4e72a3b44b24aec625d70932b756f
Gavin Weiguang Ding <gavin.w.ding@gmail.com>
2019-11-01 12:18:53 -0400

    finished linf pgd benchmark

--- advertorch_examples/attack_benchmarks/benchmark_iterative_projected_gradient.py
+++ advertorch_examples/attack_benchmarks/benchmark_iterative_projected_gradient.py
@@ -1,3 +1,54 @@
+# Copyright (c) 2018-present, Royal Bank of Canada and other authors.
+# See the AUTHORS.txt file for a list of contributors.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#
+#
+# Automatically generated benchmark report (screen print of running this file)
+#
+# sysname: Linux
+# release: 4.4.0-140-generic
+# version: #166-Ubuntu SMP Wed Nov 14 20:09:47 UTC 2018
+# machine: x86_64
+# python: 3.7.3
+# torch: 1.1.0
+# torchvision: 0.3.0
+# advertorch: 0.1.5
+
+# attack type: LinfPGDAttack
+# attack kwargs: loss_fn=CrossEntropyLoss()
+#                eps=0.15
+#                nb_iter=40
+#                eps_iter=0.01
+#                rand_init=True
+#                clip_min=0.0
+#                clip_max=1.0
+#                targeted=False
+# data: mnist_test
+# model: mnist_lenet5_clntrained
+# accuracy: 98.89%
+# robust accuracy: 8.9%
+
+# attack type: LinfPGDAttack
+# attack kwargs: loss_fn=CrossEntropyLoss()
+#                eps=0.15
+#                nb_iter=40
+#                eps_iter=0.01
+#                rand_init=True
+#                clip_min=0.0
+#                clip_max=1.0
+#                targeted=False
+# data: mnist_test
+# model: mnist_lenet5_advtrained
+# accuracy: 98.64%
+# robust accuracy: 96.93%
+
+
+import torch.nn as nn
+
 from advertorch_examples.utils import get_mnist_test_loader
 from advertorch_examples.utils import get_mnist_lenet5_clntrained
 from advertorch_examples.utils import get_mnist_lenet5_advtrained
@@ -16,22 +67,19 @@ from advertorch.attacks import LinfPGDAttack
 # TODO: from advertorch.attacks import FastFeatureAttack
 
 from advertorch_examples.benchmark_utils import benchmark_robust_accuracy
-# TODO: from advertorch_examples.benchmark_utils import benchmark_margin
 
 batch_size = 100
 device = "cuda"
 
-
 lst_attack = [
-    (LinfPGDAttack, {}),
-]
-# TODO: add dictionary values
+    (LinfPGDAttack, dict(
+        loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=0.15,
+        nb_iter=40, eps_iter=0.01, rand_init=True,
+        clip_min=0.0, clip_max=1.0, targeted=False)),
+]  # each element in the list is the tuple (attack_class, attack_kwargs)
 
-mnist_clntrained_model = get_mnist_lenet5_clntrained()
-mnist_clntrained_model.to(device)
-mnist_advtrained_model = get_mnist_lenet5_advtrained()
-mnist_advtrained_model.to(device)
-
+mnist_clntrained_model = get_mnist_lenet5_clntrained().to(device)
+mnist_advtrained_model = get_mnist_lenet5_advtrained().to(device)
 mnist_test_loader = get_mnist_test_loader(batch_size=batch_size)
 
 lst_setting = [
@@ -42,9 +90,7 @@ lst_setting = [
 
 info = get_benchmark_sys_info()
 
-
 lst_benchmark = []
-
 for model, loader in lst_setting:
     for attack_class, attack_kwargs in lst_attack:
         lst_benchmark.append(benchmark_robust_accuracy(
--- advertorch_examples/benchmark_utils.py
+++ advertorch_examples/benchmark_utils.py
@@ -9,19 +9,21 @@ from advertorch.attacks.utils import multiple_mini_batch_attack
 
 
 def get_benchmark_sys_info():
-    rval = ""
+    rval = "#\n#\n"
+    rval += ("# Automatically generated benchmark report "
+             "(screen print of running this file)\n#\n")
     uname = os.uname()
-    rval += "sysname: {}\n".format(uname.sysname)
-    rval += "release: {}\n".format(uname.release)
-    rval += "version: {}\n".format(uname.version)
-    rval += "machine: {}\n".format(uname.machine)
-    rval += "python: {}.{}.{}\n".format(
+    rval += "# sysname: {}\n".format(uname.sysname)
+    rval += "# release: {}\n".format(uname.release)
+    rval += "# version: {}\n".format(uname.version)
+    rval += "# machine: {}\n".format(uname.machine)
+    rval += "# python: {}.{}.{}\n".format(
         sys.version_info.major,
         sys.version_info.minor,
         sys.version_info.micro)
-    rval += "torch: {}\n".format(torch.__version__)
-    rval += "torchvision: {}\n".format(torchvision.__version__)
-    rval += "advertorch: {}\n".format(advertorch.__version__)
+    rval += "# torch: {}\n".format(torch.__version__)
+    rval += "# torchvision: {}\n".format(torchvision.__version__)
+    rval += "# advertorch: {}\n".format(advertorch.__version__)
     return rval
 
 
@@ -34,11 +36,18 @@ def benchmark_robust_accuracy(
     robust_accuracy = 100. * (label == advpred).sum().item() / len(label)
 
     rval = ""
-    rval += "attack type: {}\n".format(attack_class.__name__)
-    rval += "attack kwargs: {}\n".format(str(attack_kwargs))
-    rval += "data: {}\n".format(loader.name)
-    rval += "model: {}\n".format(model.name)
-    rval += "accuracy: {}%\n".format(accuracy)
-    rval += "robust accuracy: {}%\n".format(robust_accuracy)
+    rval += "# attack type: {}\n".format(attack_class.__name__)
+
+    prefix = " attack kwargs: "
+    count = 0
+    for key in attack_kwargs:
+        this_prefix = prefix if count == 0 else " " * len(prefix)
+        count += 1
+        rval += "#{}{}={}\n".format(this_prefix, key, attack_kwargs[key])
+
+    rval += "# data: {}\n".format(loader.name)
+    rval += "# model: {}\n".format(model.name)
+    rval += "# accuracy: {}%\n".format(accuracy)
+    rval += "# robust accuracy: {}%\n".format(robust_accuracy)
 
     return rval
