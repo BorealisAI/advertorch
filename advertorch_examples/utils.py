2a8673c919790d5a1aae61d73d1d639e6cae28f5
Gavin Weiguang Ding <gavin.w.ding@gmail.com>
2020-02-23 16:22:08 -0500

    added utils

--- advertorch_examples/utils.py
+++ advertorch_examples/utils.py
@@ -13,6 +13,7 @@ from __future__ import unicode_literals
 import os
 import sys
 import pathlib
+from torch.utils.data.dataset import Subset
 
 import numpy as np
 import torch
@@ -91,6 +92,82 @@ def get_mnist_lenet5_advtrained():
     model.name = "MNIST LeNet 5 PGD training according to Madry et al. 2018"
     # TODO: also described where can you find this model, and how is it trained
     return model
+
+
+def get_madry_et_al_cifar10_train_transform():
+    return transforms.Compose([
+        transforms.Pad(4, padding_mode="reflect"),
+        transforms.RandomCrop(32),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
+
+
+
+def get_train_val_loaders(
+        dataset, datapath=DATA_PATH,
+        train_size=None, val_size=5000,
+        train_batch_size=100, val_batch_size=1000,
+        kwargs=None, train_transform=None, val_transform=None,
+        train_shuffle=True, val_shuffle=False):
+    """Support MNIST and CIFAR10"""
+    if kwargs is None:
+        kwargs = {}
+    if train_transform is None:
+        train_transform = transforms.ToTensor()
+    if val_transform is None:
+        val_transform = transforms.ToTensor()
+
+    datapath = os.path.join(datapath, dataset)
+
+    trainset = datasets.__dict__[dataset](
+        datapath, train=True, download=True, transform=train_transform)
+
+    if train_size is not None:
+        assert train_size + val_size <= len(trainset)
+
+    if val_size > 0:
+        indices = list(range(len(trainset)))
+        trainset = Subset(trainset, indices[val_size:])
+
+        valset = datasets.__dict__[dataset](
+            datapath, train=True, download=True, transform=val_transform)
+        valset = Subset(valset, indices[:val_size])
+        val_loader = torch.utils.data.DataLoader(
+            valset, batch_size=val_batch_size, shuffle=val_shuffle, **kwargs)
+
+    else:
+        val_loader = None
+
+    if train_size is not None:
+        trainset = Subset(trainset, list(range(train_size)))
+
+    train_loader = torch.utils.data.DataLoader(
+        trainset, batch_size=train_batch_size, shuffle=train_shuffle, **kwargs)
+
+    return train_loader, val_loader
+
+
+def get_test_loader(
+        dataset, datapath=DATA_PATH, test_size=None, batch_size=1000,
+        transform=None, kwargs=None, shuffle=False):
+    """Support MNIST and CIFAR10"""
+    if kwargs is None:
+        kwargs = {}
+    if transform is None:
+        transform = transforms.ToTensor()
+
+    datapath = os.path.join(datapath, dataset)
+
+    testset = datasets.__dict__[dataset](
+        datapath, train=False, download=True, transform=transform)
+
+    if test_size is not None:
+        testset = Subset(testset, list(range(test_size)))
+
+    test_loader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=shuffle, **kwargs)
+    return test_loader
 
 
 def bchw2bhwc(x):
